a7f0679394540bc1141e6ab34a31ef0b0daf82eb
GiTheo2 <theodaude24@gmail.com>
2026-02-22 12:51:54 +0100

    MAITRE CLAUDE, mais que 10 lignes de Claude
    G corrigé le fait qu'on mettait les donnees avec champion a deviner dans le bloc alors qu'on voulait le champion entré. Le code marche maintenant, il nous reste encore à faire entrer les blocs dans des cases

--- script.js
+++ script.js
@@ -2,10 +2,14 @@
 let championADeviner = null;
 let championEntre = null
 let pointsCommuns = null
+let champions = []
 // Charger le fichier JSON
 fetch('./data/champions.json')
     .then(response => response.json())  // Convertir la réponse en JSON
-    .then(champions => {
+    //Claude nous a dit de remplacer .then champions par then data, puis de
+    //dire ensuite que data = champions
+    .then(data => {
+        champions = data;
         // Récupérer l'élément HTML où on va afficher les noms
         const championsList = document.getElementById('champions-list');
 
@@ -16,16 +20,27 @@ fetch('./data/champions.json')
         // Pour vérifier que ça marche (à retirer plus tard)
         console.log('Champion à deviner:', championADeviner.name[0]);
 
-        // Plus tard, on ajoutera ici le système de devinettes
+        // Plus ta  rd, on ajoutera ici le système de devinettes
         creerBlocs()
     })
     .catch(error => {
         console.error('Erreur lors du chargement:', error);
     });
+//Ce qui suit, c'est claude
 function getVal() {
-    var championEntre = document.getElementById("maBarre").value;
-    console.log(championEntre);
+    var nomEntre = document.getElementById("maBarre").value;
+    championEntre = champions.find(c => c.name[0].toLowerCase() === nomEntre.toLowerCase());
+    if (championEntre) {
+        comparer();
+    } else {
+        alert("Champion introuvable !");
+    }
 }
+//ancien code:
+//    function getVal() {
+//    var championEntre = document.getElementById("maBarre").value;
+//    console.log(championEntre);
+//}
 
 //fonction qui va comparer les caracteristiques du champion entré vs champion à deviner
 // puis qui va retourner vert, orange, rouge en fonction des points communs de ces deux champions
@@ -59,7 +74,7 @@ function comparer() {
 
         if (pointsCommuns === championADeviner[q].length) {
             let bloc = document.createElement('div');
-            bloc.innerText = championADeviner[q];
+            bloc.innerText = championEntre[q];
             bloc.style.width = '100px';
             bloc.style.height = '100px';
             bloc.style.border = '1px solid black';
@@ -70,7 +85,7 @@ function comparer() {
         }
         else if (pointsCommuns > 0 && pointsCommuns < championADeviner[q].length) {
             let bloc = document.createElement('div');
-            bloc.innerText = championADeviner[q];
+            bloc.innerText = championEntre[q];
             bloc.style.width = '100px';
             bloc.style.height = '100px';
             bloc.style.border = '1px solid black';
@@ -81,7 +96,7 @@ function comparer() {
         }
         else {
             let bloc = document.createElement('div');
-            bloc.innerText = championADeviner[q];
+            bloc.innerText = championEntre[q];
             bloc.style.width = '100px';
             bloc.style.height = '100px';
             bloc.style.border = '1px solid black';
